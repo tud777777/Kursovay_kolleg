76f089530939776f57fd4cd79516f6bd7d5f24a6
tud777777 <tud777777@yandex.ru>
2025-02-11 10:13:33 +0300

    123

--- public/script.js
+++ public/script.js
@@ -3,11 +3,47 @@ let catalog = document.querySelector('.nav_menu_catalog')
 let navMenu = document.querySelector('.nav_menu')
 window.addEventListener("scroll", () => {
     const topOffset = navMenu.getBoundingClientRect().top;
-    if (topOffset == 0) {
+    if (topOffset == -1) {
         logo.classList.remove("none");
         catalog.style.margin = '0 187.5px 0 0'
     } else {
         logo.classList.add("none");
         catalog.style.margin = '0 272.5px 0 0'
     }
+})
+
+let slides = document.querySelectorAll('.slide_stocks')
+let left = document.querySelector('.main_slider_stocks_button_left')
+let right = document.querySelector('.main_slider_stocks_button_right')
+let counter = 0;
+let count = slides.length
+console.log(count)
+right.addEventListener('click', function () {
+    counter+=100
+    if(counter==count*100){
+        counter = 0
+        slides.forEach(slide => {
+            slide.style.transform = "translateX(-"+counter+"%)"
+        })
+    }
+    else{
+        slides.forEach(slide => {
+            slide.style.transform = "translateX(-"+counter+"%)"
+        })
+    }
+    
+})
+left.addEventListener('click', function () {
+    counter-=100
+    if(counter<0){
+        counter = (count-1)*100
+        slides.forEach(slide => {
+            slide.style.transform = "translateX(-"+counter+"%)"
+        })
+    }
+    else{
+        slides.forEach(slide => {
+            slide.style.transform = "translateX(-"+counter+"%)"
+        })
+    }       
 })
