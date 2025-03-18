87b9ad12a52743ce85a5ada491af967c3bfdb875
tud777777 <tud777777@yandex.ru>
2025-03-18 16:07:26 +0300

    Kursovaya kolledg

--- build_project/app/Http/Controllers/ProjectController.php
+++ build_project/app/Http/Controllers/ProjectController.php
@@ -98,19 +98,49 @@ class ProjectController extends Controller
             'description' => 'required|max:255',
             'end_date' => 'required|date',
         ]);
-        Project::query()->where('id','=', $validated['projectId'])->update([$validated['name'], $validated['description'], $validated['end_date']]);
+        Project::query()->where('id','=', $validated['projectId'])->update(['name' => $validated['name'], 'description' => $validated['description'],'end_date' => $validated['end_date']]);
         $project = Project::query()->where('id','=', $validated['projectId'])->first();
-        $materials_for_project = MaterialForProject::query()->where('project_id','=', $validated['id'])->get();
-        $equipments_for_project = EquipmentForProject::query()->where('project_id','=', $validated['id'])->get();
-        $construction_crews_for_project = ConstructionCrewForProject::query()->where('project_id','=', $validated['id'])->get();
-        foreach ($materials_for_project as $material_for_project) {
-            foreach ($validated['materials'] as $material) {
-                if ($material['id'] != $material_for_project['id']) {
-                    $project->create_materials()->attach($material['id'], ['count' => $material['count']]);
-                }
+        $materials_for_project = MaterialForProject::query()->where('project_id','=', $validated['projectId'])->get();
+        $equipments_for_project = EquipmentForProject::query()->where('project_id','=', $validated['projectId'])->get();
+        $construction_crews_for_project = ConstructionCrewForProject::query()->where('project_id','=', $validated['projectId'])->get();
+        $materials_unique = [];
+        $diff1 = array_diff($validated['materials'], $materials_for_project);
+        $diff2 = array_diff($materials_for_project, $validated['materials']);
+        $materials_unique = array_merge($diff1, $diff2);
+        foreach ($materials_unique as $material_unique) {
+            $material = MaterialForProject::query()->where('id','=', $material_unique->id)->first();
+            if(empty($material)){
+                $project->create_materials()->attach($material_unique['id'], ['count' => $material_unique['count']]);
             }
-
+            else{
+                $material->delete();
+            }
         }
+        $equipments_unique = array_unique($validated['equipments'], $equipments_for_project);
+        foreach ($equipments_unique as $equipment_unique) {
+            $equipment = MaterialForProject::query()->where('id','=', $equipment_unique->id)->first();
+            if(empty($equipment)){
+                $project->create_equipments()->attach($equipment_unique['id'], ['count' => $equipment_unique['count']]);
+            }
+            else{
+                $equipment->delete();
+            }
+        }
+        $construction_crews_unique = array_unique($validated['construction_crews'], $construction_crews_for_project);
+        foreach ($construction_crews_unique as $construction_crew_unique) {
+            $construction_crew = MaterialForProject::query()->where('id','=', $construction_crew_unique->id)->first();
+            if(empty($construction_crew)){
+                $project->create_equipments()->attach($construction_crew_unique['id'], ['count' => $construction_crew_unique['count']]);
+            }
+            else{
+                $construction_crew->delete();
+            }
+        }
+        $project = Project::with('owner')->where('id','=', $validated['projectId'])->first();
+        $materials = MaterialForProject::with('material')->where('project_id','=', $validated['projectId'])->get();
+        $equipments = EquipmentForProject::with('equipment')->where('project_id','=', $validated['projectId'])->get();
+        $construction_crews = ConstructionCrewForProject::with('construction_crew')->where('project_id','=', $validated['projectId'])->get();
+        return Inertia::render('Projects/ShowProject', ['project' => $project, 'equipments' => $equipments, 'construction_crews' => $construction_crews, 'materials' => $materials]);
     }
     public function show_project(Request $request)
     {
